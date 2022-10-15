dd90b432ad1ec505f6196b3597ab0a7c39883eaf
jeelpatel231 <jeelpatel231@gmail.com>
2022-10-15 20:07:07 +0530

    remove redundant create method

--- user/serializers.py
+++ user/serializers.py
@@ -6,8 +6,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
-    def create(self, validated_data):
-        instance = self.Meta.model(**validated_data)
-        instance.save()
-        return instance
