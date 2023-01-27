843e006847b3d91b76e3c09eb794e4e916589260
jeelpatel231 <jeelpatel231@gmail.com>
2023-01-27 17:28:31 +0000

    add new models and register them in admin panel

--- user/models.py
+++ user/models.py
@@ -1,19 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager as UserManager_
+from django.contrib.auth.models import AbstractUser
 from typing import Optional, Iterable
 
 # from user import managers
-from user.roles import Role
+from user.roles import Role, VolunteerType, CompanyType, Gender
 from django.contrib.auth.models import Permission
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from typing import List, Any
+from functools import partial
 
 
-def number_validator(value: Any) -> None:
-    if len(value) != 10:
+def number_validator(value: Any, length: int) -> None:
+    if len(value) != length:
         raise ValidationError(
-            _("Phone Number must be 10 digit long"),
+            _(f"Number must be {length} digit long"),
         )
     if not all(i.isdigit() for i in value):
         raise ValidationError(_("Only numbers are allowed"))
@@ -30,11 +31,11 @@ class User(AbstractUser):
     Hence, the _base_role is Undefined, Roles can be found at `users.roles.Role`
     """
 
-    _base_role = Role.UNDEFINED
+    _base_role: Role = Role.UNDEFINED
     _predefined_permissions: List[str] = []
 
     role = models.PositiveSmallIntegerField(choices=Role.choices)
-    phone_number = models.CharField(max_length=10, validators=[number_validator])
+    phone_number = models.CharField(max_length=10, validators=[partial(number_validator, length=10)])
 
     def save(
         self,
@@ -55,6 +56,8 @@ class User(AbstractUser):
 class Admin(User):
     _base_role = Role.ADMIN
 
+    institute = models.CharField(max_length=256)
+
     class Meta:
         verbose_name = "Admin"
         verbose_name_plural = "Admins"
@@ -74,6 +77,11 @@ class Admin(User):
 class Student(User):
     _base_role = Role.STUDENT
     marks = models.IntegerField()
+    institute = models.CharField(max_length=256)
+    department = models.CharField(max_length=256)
+    semester = models.CharField(max_length=1, blank=False)
+    batch_year = models.DateField() # 4 character field possible
+
     _predefined_permissions = ["view_student"]
 
     class Meta:
@@ -84,6 +92,11 @@ class Student(User):
 class Volunteer(User):
     _base_role = Role.VOLUNTEER
     job_numbers = models.PositiveSmallIntegerField()
+    department = models.CharField(max_length=256)
+    semester = models.CharField(max_length=1,blank=False)
+    volunteer_type = models.PositiveSmallIntegerField(choices=VolunteerType.choices)
+    reference = models.TextField(max_length=2000, null=True)
+
     _predefined_permissions = ["view_volunteer"]
 
     class Meta:
@@ -93,9 +106,49 @@ class Volunteer(User):
 
 class DeptOfficer(User):
     _base_role = Role.DEPT_OFFICER
-    dept = models.TextField()
+    department = models.CharField(max_length=256)
+    address = models.TextField(max_length=2000)
+
     _predefined_permissions = ["view_deptofficer"]
 
     class Meta:
         verbose_name = "Department Officer"
         verbose_name_plural = "Department Officers"
+
+class Company(models.Model):
+    name = models.CharField(max_length=256)
+    email_id = models.EmailField()
+    phone_number = models.CharField(max_length=10, validators=[partial(number_validator, length=10)])
+    hr_name = models.CharField(max_length=256)
+    address = models.TextField()
+    company_relation = models.PositiveSmallIntegerField(choices=CompanyType.choices)
+    # is mandatory in document but ONLY needed if company is a child
+    parent_company_name = models.CharField(max_length=256, null=True)
+    # possible to move to enum probably
+    industry_type = models.CharField(max_length=256) 
+    current_employees = models.PositiveBigIntegerField()
+    # possible to move to enum probably
+    company_type = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+class CurrentOpening(models.Model):
+    job_title = models.CharField(max_length=256)
+    opening_year = models.CharField(max_length=256)
+    # supposed to be numeric 15? what exactly is nature, in a 15 digit number? 
+    nature_of_job = models.CharField(max_length=15, validators=[partial(number_validator, length = 15)])
+    short_description = models.CharField(max_length=256)
+    long_description = models.TextField()
+    min_qualification = models.PositiveIntegerField()
+    specialization = models.CharField(max_length=256)
+    special_skill_requirement = models.TextField()
+    vacancy_count = models.PositiveIntegerField()
+    # why number? possibly text about address
+    vacancy_location = models.PositiveIntegerField()
+    min_package = models.PositiveBigIntegerField()
+    max_package = models.PositiveBigIntegerField()
+    # supposed to be boolean, what about `ANY` type? nullable boolean?
+    gender_preference = models.PositiveSmallIntegerField(choices=Gender.choices)
+
--- user/roles.py
+++ user/roles.py
@@ -1,8 +1,25 @@
 from django.db.models import IntegerChoices
 
+
 class Role(IntegerChoices):
-    ADMIN =         1, 'Admin'
-    STUDENT =       2, 'Student'
-    VOLUNTEER =     3, 'Volunteer'
-    DEPT_OFFICER =  4, 'Department Officer'
-    UNDEFINED =     5, 'Undefined'
+    ADMIN = 1, "Admin"
+    STUDENT = 2, "Student"
+    VOLUNTEER = 3, "Volunteer"
+    DEPT_OFFICER = 4, "Department Officer"
+    UNDEFINED = 5, "Undefined"
+
+
+class VolunteerType(IntegerChoices):
+    LEADER = 1, "Leader"
+    WORKER = 2, "Worker"
+
+
+class CompanyType(IntegerChoices):
+    PARENT = 1, "Parent"
+    CHILD = 2, "Child"
+
+
+class Gender(IntegerChoices):
+    MALE = 1, "Male"
+    FEMALE = 2, "Female"
+    ANY = 3, "Any"
